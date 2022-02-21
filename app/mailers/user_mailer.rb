8acff71dbcaa5c29e4f078fd54e0b77efb34501f
davnguyen <dav-ngu31@hotmail.fr>
2022-02-21 14:43:02 +0100

    fourth commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'no-reply@monsite.fr'
+    default from: 'dav-ngu31@hotmail.fr'
  
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
